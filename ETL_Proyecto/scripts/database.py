d082a0d384d5c07b0469a91c5c1398be256de116
Juan José Claros González <jjclaros-2022a@corhuila.edu.co>
2026-04-24 16:06:11 -0500

    cambia script

--- ETL_Proyecto/scripts/database.py
+++ ETL_Proyecto/scripts/database.py
@@ -1,29 +1,20 @@
-import os
-from sqlalchemy import create_engine
+import streamlit as st
 from urllib.parse import quote_plus
-from dotenv import load_dotenv
-
-# Cargar variables del .env
-load_dotenv()
+from sqlalchemy import create_engine
 
 def get_engine():
-    # 🔐 Leer credenciales
-    DB_HOST = os.getenv("DB_HOST")
-    DB_PORT = os.getenv("DB_PORT")
-    DB_USER = os.getenv("DB_USER")
-    DB_PASSWORD = os.getenv("DB_PASSWORD")
-    DB_NAME = os.getenv("DB_NAME")
+    DB_HOST = st.secrets.get("DB_HOST")
+    DB_PORT = st.secrets.get("DB_PORT")
+    DB_USER = st.secrets.get("DB_USER")
+    DB_PASSWORD = st.secrets.get("DB_PASSWORD")
+    DB_NAME = st.secrets.get("DB_NAME")
 
-    # 🧱 Construir URL de conexión
+    if not all([DB_HOST, DB_PORT, DB_USER, DB_PASSWORD, DB_NAME]):
+        raise ValueError("❌ Faltan variables en st.secrets")
+
     DATABASE_URL = (
         f"postgresql://{quote_plus(DB_USER)}:{quote_plus(DB_PASSWORD)}"
         f"@{DB_HOST}:{DB_PORT}/{DB_NAME}?sslmode=require"
     )
 
-    # ⚙️ Crear engine
-    engine = create_engine(
-        DATABASE_URL,
-        pool_pre_ping=True
-    )
-
-    return engine
+    return create_engine(DATABASE_URL)
