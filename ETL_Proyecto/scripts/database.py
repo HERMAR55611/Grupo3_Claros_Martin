1aefbfd5fff1134fd7a34855f4c8ce6e0a98b912
Juan José Claros González <jjclaros-2022a@corhuila.edu.co>
2026-04-24 19:10:33 -0500

    notebook jupyter

--- ETL_Proyecto/scripts/database.py
+++ ETL_Proyecto/scripts/database.py
@@ -1,20 +1,23 @@
-import streamlit as st
+import os
 from urllib.parse import quote_plus
 from sqlalchemy import create_engine
+from dotenv import load_dotenv
+
+load_dotenv()
 
 def get_engine():
-    DB_HOST = st.secrets.get("DB_HOST")
-    DB_PORT = st.secrets.get("DB_PORT")
-    DB_USER = st.secrets.get("DB_USER")
-    DB_PASSWORD = st.secrets.get("DB_PASSWORD")
-    DB_NAME = st.secrets.get("DB_NAME")
+    DB_HOST = os.getenv("DB_HOST")
+    DB_PORT = os.getenv("DB_PORT")
+    DB_USER = os.getenv("DB_USER")
+    DB_PASSWORD = os.getenv("DB_PASSWORD")
+    DB_NAME = os.getenv("DB_NAME")
 
     if not all([DB_HOST, DB_PORT, DB_USER, DB_PASSWORD, DB_NAME]):
-        raise ValueError("❌ Faltan variables en st.secrets")
+        raise ValueError("❌ Faltan variables en .env")
 
     DATABASE_URL = (
         f"postgresql://{quote_plus(DB_USER)}:{quote_plus(DB_PASSWORD)}"
-        f"@{DB_HOST}:{DB_PORT}/{DB_NAME}?sslmode=require"
+        f"@{DB_HOST}:{DB_PORT}/{DB_NAME}"
     )
 
     return create_engine(DATABASE_URL)
