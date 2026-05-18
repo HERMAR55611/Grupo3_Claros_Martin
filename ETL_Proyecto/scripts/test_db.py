dd0acf0af41b38c76cb7ceded991247cd2a8974e
HERMAR55611 <hamartin202021@corhuila.edu.co>
2026-05-17 22:25:56 -0500

    Limpieza repositorio proyecto final

--- ETL_Proyecto/scripts/test_db.py
+++
@@ -1,8 +0,0 @@
-from scripts.database import get_connection
-
-try:
-    conn = get_connection()
-    print("✅ Conexión exitosa a Supabase")
-    conn.close()
-except Exception as e:
-    print("❌ Error:", e)
